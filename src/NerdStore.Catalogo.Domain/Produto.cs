7f199be1cb39c6fc8222d028c8df8d4bce052bf0
Luiz C Dazzini <lcdazzini@gmail.com>
2020-05-13 22:08:14 -0300

    Criação de Classe DomainException e Validações

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -40,13 +40,50 @@ namespace NerdStore.Catalogo.Domain
 			CategoriaId = categoriaId;
 			DataCadastro = dataCadastro;
 			Imagem = imagem;
-			QuantidadeEstoque = quantidadeEstoque;
 
+			Validar();
 		}
-	}
 
-	public class Categoria : Entity
-	{
+		public void Ativar() => Ativo = true;
 
+		public void Desativar() => Ativo = false;
+
+		public void AlterarCatergoria(Categoria categoria)
+		{
+			Categoria = categoria;
+			CategoriaId = categoria.Id;
+		}
+
+		public void AlterarDescricao(string descricao)
+		{
+			Descricao = descricao;
+		}
+
+		public void DebitarEstoque(int quantidade)
+		{
+			if (quantidade < 0) quantidade *= -1;
+			QuantidadeEstoque -= quantidade;
+		}
+
+		public void ReporEstoque(int quantidade)
+		{
+			QuantidadeEstoque += quantidade;
+		}
+
+		public bool PossuiEstoque(int quantidade)
+		{
+			return QuantidadeEstoque >= quantidade;
+		}
+
+		public void Validar()
+		{
+			//FluentValidation
+
+			Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+			Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
+			Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+			Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
+			Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
+		}
 	}
 }
